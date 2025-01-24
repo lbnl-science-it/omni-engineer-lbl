d788a55202243795f87bb0cbcb5ec66f3cc155e8
tyfong-lbl <tyfong@lbl.gov>
2025-01-23 16:45:14 -0800

    Fix version of module

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 from colorama import init, Fore, Back, Style
 import difflib
 import asyncio
-from duckduckgo_search import AsyncDDGS as DDGS
+from duckduckgo_search import AsyncDDGS 
 import json
 from pygments import highlight
 from pygments.lexers import get_lexer_by_name
